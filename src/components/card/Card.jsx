82627f582fad7b6c24304effd7d3b8d02bf88d62
Pearl Patel <pearlpatel052@gmail.com>
2024-12-04 16:00:40 +0530

    All Tasks completed

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import './styles.scss'
+import { toJpeg } from "html-to-image";
+
 const Card = ({index, university, link}) => {
+  function downloadCard(cardId) {
+      const card = document.getElementById(cardId);
+      toJpeg(card, { quality: 0.95 }).then((dataUrl) => {
+        const link = document.createElement("a");
+        link.href = dataUrl;
+        link.download = `${cardId}.jpeg`;
+        link.click();
+      });
+  }
   return (
-    <div className='card' id={index}>
+    <div className='card' id='index'>
       <a href={link[0]} target='_blank'>
         <div className='university-text'>
           {university}
         </div>
       </a>
-      <button>Download</button>
+      <button onClick={() => downloadCard(index)}>Download</button>
     </div>
   )
 }
